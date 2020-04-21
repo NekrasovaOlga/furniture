13a003b009c61cc38f978a1b18095ca22faddf90
LisenkoKalina <nekrasova.kalina@yandex.ru>
2020-04-21 18:13:50 +0300

    Добавлен блок слайдер

---
+++ app/js/main.js
@@ -0,0 +1,10 @@
+$('.lookbook').slick({
+    centerMode: true,
+    slidesToShow: 1,
+    dots: true,
+    dotsClass: 'lookbook-dots',
+    arrows: false,
+    infinite: true,
+    variableWidth: true,
+    speed: 300,
+});
